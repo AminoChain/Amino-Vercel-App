4950998ffd74c97ca43a02b50576efd33d7f076e
MaximilianFullStack <mmillionam@gmail.com>
2022-11-05 18:30:56 -0400

    new file:   assets/arrowRight.png 	new file:   assets/coinbaseLogo.png 	new file:   assets/metamaskLogo.png 	new file:   assets/search.png 	new file:   assets/walletConnectLogo.png 	modified:   components/LandingPage/LandingPage.jsx 	modified:   components/donate/DonateBanner.jsx 	new file:   components/donate/DonateFooter.jsx 	modified:   components/donate/DonateNav.jsx 	modified:   components/donate/DonatePage.jsx 	modified:   components/donate/donateProgress/DonateSearch.jsx 	modified:   components/donate/donateProgress/DonateSearchSteps.jsx 	modified:   components/donate/donateProgress/DonateStart.jsx

--- components/donate/donateProgress/DonateSearchSteps.jsx
+++ components/donate/donateProgress/DonateSearchSteps.jsx
@@ -1,43 +1,44 @@
+const DonateSearchSteps = ({ progress, barPercent, setBarPercent }) => {
+  const bar = (progress / 10) * 100
 
-
-
-const DonateSearchSteps = ({progress, barPercent, setBarPercent}) => {
-  const bar = (progress/10) * 100
-  
   return (
-    <div className="w-full flex flex-col">
+    <div className="w-full flex flex-col pt-[1rem]">
       <div className="w-full flex flex-row px-20">
         <div className="w-full flex flex-row items-start">
-          <div className="flex justify-center items-center bg-main text-white h-10 w-10  rounded-full">
+          <div className="flex justify-center items-center bg-main text-white text-base h-10 w-10 font-satoshiMedium rounded-full">
             1
           </div>
           <div className="flex flex-col justify-end text-black py-2 px-4">
-            <div className="text-lg">Book Appointment</div>
-            <div className="text-sm">
+            <div className="text-base font-satoshiMedium text-black">
+              Book Appointment
+            </div>
+            <div className="text-sm font-satoshiMedium text-black">
               Choose a donation center as per your preference
             </div>
           </div>
         </div>
         <div className="w-full flex flex-row items-start">
-          <div className="flex justify-center items-center bg-main text-white h-10 w-10  rounded-full">
+          <div className="flex justify-center items-center bg-white text-black text-base border border-main font-satoshiMedium h-10 w-10 rounded-full">
             2
           </div>
           <div className="flex flex-col justify-end text-black py-2 px-4">
-            <div className="text-lg text-main">
+            <div className="text-base text-main font-satoshiMedium">
               Visit center and verify donation
             </div>
-            <div className="text-sm">
+            <div className="text-sm font-satoshiMedium text-black">
               Donate Stem Cells at the selected center
             </div>
           </div>
         </div>
       </div>
-      <div className="w-full px-20">
-      <div className="w-full h-2 bg-slate-200 rounded">
-        <div style={{width :`${bar}%`}} className={`h-2 bg-primary rounded`}/>
+      <div className="w-full px-20 pt-[1rem]">
+        <div className="w-full h-2 bg-slate-200 rounded">
+          <div
+            style={{ width: `${bar}%` }}
+            className={`h-2 bg-primary rounded`}
+          />
+        </div>
       </div>
-      </div>
-      
     </div>
   )
 }
