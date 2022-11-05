d8d92a20524d36e20521b5dba9ca14c65eeeb84d
MaximilianFullStack <mmillionam@gmail.com>
2022-11-04 23:57:39 -0400

    modified:   pages/index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import LandingPage from '../components/landingpage/LandingPage'
+import LandingPage from '../components/landingPage/LandingPage'
 
 export const getStaticProps = async () => {
   const feed = [
