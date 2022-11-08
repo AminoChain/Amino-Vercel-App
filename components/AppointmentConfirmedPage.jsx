0d3fe3d9bcf75639046e5b3bbec84a3504b943e4
Casen Luck <0xcasen@gmail.com>
2022-11-07 22:06:14 -0500

    added'

---
+++ components/AppointmentConfirmedPage.jsx
@@ -0,0 +1,17 @@
+import DonateFooter from './donate/DonateFooter'
+import DonateNav from './donate/DonateNav'
+import DonateSearchSteps from './donate/donatesearch/DonateSearchSteps'
+import AppointmentInstructions from './donate/donatesearch/donateSchedule/appointmentConfirmed/AppointmentInstructions'
+
+const AppointmentConfirmedPage = ({ bioBank, progress }) => {
+  return (
+    <div className="w-full flex flex-col">
+      <DonateNav />
+      <DonateSearchSteps progress={progress} />
+      <AppointmentInstructions bioBank={bioBank} />
+      <DonateFooter />
+    </div>
+  )
+}
+
+export default AppointmentConfirmedPage
--- pages/donate/biobank/appointmentConfirmed/index.jsx
+++ pages/donate/biobank/appointmentConfirmed/index.jsx
@@ -1,30 +1,29 @@
-import { useState } from "react"
-import DonateBanner from "../../../../components/donate/DonateBanner"
-import AppointmentConfirmedPage from "../../../../components/donate/donatesearch/donateSchedule/appointmentConfirmed/AppointmentConfirmedpage"
-import prisma from "../../../../lib/prisma"
+import { useState } from 'react'
+import DonateBanner from '../../../../components/donate/DonateBanner'
+import AppointmentConfirmedPage from '../../../../components/AppointmentConfirmedPage'
+import prisma from '../../../../lib/prisma'
 
 export async function getServerSideProps(context) {
   const { bioBankId } = context.query
 
   const bioBank = await prisma.bioBank.findUnique({
     where: {
-      id: bioBankId
+      id: bioBankId,
     },
-  })  
+  })
 
   return {
     props: { bioBank: bioBank }, // will be passed to the page component as props
   }
 }
 
-const AppointmentConfirmed = ({bioBank}) => {
+const AppointmentConfirmed = ({ bioBank }) => {
   const [progress, setProgress] = useState(4)
 
-
-  return(
+  return (
     <div className="w-screen">
-      <DonateBanner/>
-      <AppointmentConfirmedPage progress={progress} bioBank={bioBank}/>
+      <DonateBanner />
+      <AppointmentConfirmedPage progress={progress} bioBank={bioBank} />
     </div>
   )
 }
