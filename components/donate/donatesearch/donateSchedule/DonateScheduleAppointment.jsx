3799b00b1ee1a2a0fdb69e12c634c95e2239581f
Casen Luck <0xcasen@gmail.com>
2022-11-07 19:09:36 -0500

    added keys

--- components/donate/donatesearch/donateSchedule/DonateScheduleAppointment.jsx
+++ components/donate/donatesearch/donateSchedule/DonateScheduleAppointment.jsx
@@ -71,7 +71,7 @@ const DonateScheduleAppointment = ({ bioBank }) => {
                     className="border p-2 rounded font-satoshiRegular text-black"
                   >
                     {months.map((element) => {
-                      return <option value={element}>{element}</option>
+                      return <option  key={element} value={element}>{element}</option>
                     })}
                   </select>
                   <select
@@ -80,7 +80,7 @@ const DonateScheduleAppointment = ({ bioBank }) => {
                     className="border p-2 mx-2 rounded font-satoshiRegular text-black"
                   >
                     {days.map((element) => {
-                      return <option value={element}>{element}</option>
+                      return <option key={element} value={element}>{element}</option>
                     })}
                   </select>
                 </div>
