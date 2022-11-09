a33bb7d880eac42d8efe6f847ec3561108890836
Casen Luck <0xcasen@gmail.com>
2022-11-08 22:59:31 -0500

    git come on

---
+++ components/biobank/BioBankPage.jsx
@@ -0,0 +1,17 @@
+import BioBankNav from '../../BioBankNav'
+import BioBankBanner from '../../BioBankBanner'
+import BioBankAppointments from '../../appointments/BioBankAppointments'
+import BioBankFooter from '../../BioBankFooter'
+
+const BioBankPage = () => {
+  return (
+    <div className="w-screen">
+      <BioBankBanner />
+      <BioBankNav />
+      <BioBankAppointments />
+      <BioBankFooter />
+    </div>
+  )
+}
+
+export default BioBankPage
