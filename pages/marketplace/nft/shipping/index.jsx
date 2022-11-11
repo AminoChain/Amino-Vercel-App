9b109a421a29fd235122ffbac8ad7bccc729dabd
cluck135 <0xcasen@gmail.com>
2022-11-10 22:50:20 -0500

    added working shipping form submits to database

--- pages/marketplace/nft/shipping/index.jsx
+++ pages/marketplace/nft/shipping/index.jsx
@@ -1,9 +1,57 @@
-import NftShippmentPage from '../../../../components/marketplace/marketplacePage/nftPath/shipping/NftShippmentPage'
+import NftShipmentPage from '../../../../components/marketplace/marketplacePage/nftPath/shipping/NftShipmentPage'
+import { gql, useQuery } from '@apollo/client'
 
-const MarketplaceShipping = () => {
+export async function getServerSideProps(context) {
+  const { tokenId } = context.query
+
+  return {
+    props: { tokenId: tokenId }, // will be passed to the page component as props
+  }
+}
+
+const MarketplaceShipping = ({tokenId}) => {
+  const GET_TOKEN_DATA = gql`
+    query Nft($tokenId: Int!) {
+      existingTokenIds(where: { tokenId: $tokenId }) {
+        tokenId
+        price
+        sizeInCC
+        donor
+        bioBank
+        hlaHashes {
+          hlaHashed_A
+          hlaHashed_B
+          hlaHashed_C
+          hlaHashed_DPB
+          hlaHashed_DRB
+        }
+      }
+    }
+  `
+
+  let id = Number(tokenId)
+  const {
+    loading,
+    error,
+    data: NftData,
+  } = useQuery(GET_TOKEN_DATA, {
+    variables: { tokenId: id },
+  })
+
+  if (loading) {
+    return (
+      <div>
+        <h2>Loading...</h2>
+      </div>
+    )
+  }
+
+  if (error) return `Error! ${error}`
+
+
   return (
     <div>
-      <NftShippmentPage />
+      <NftShipmentPage NftData={NftData} />
     </div>
   )
 }
