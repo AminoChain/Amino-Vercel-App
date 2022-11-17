9e4de5db45a9c0b4081d3808a57f46483c77c981
MaximilianFullStack <mmillionam@gmail.com>
2022-11-17 00:05:02 -0500

    modified:   components/biobank/appointments/BioBankAppointment.jsx 	modified:   components/biobank/history/BioBankHistory.jsx 	modified:   components/biobank/history/BioBankHistoryList.jsx

--- components/biobank/history/BioBankHistoryList.jsx
+++ components/biobank/history/BioBankHistoryList.jsx
@@ -1,7 +1,51 @@
 import BioBankHistory from './BioBankHistory'
+import { gql, useQuery } from '@apollo/client'
 
 const BioBankHistoryList = () => {
-  let appointments = [1, 2, 3, 4, 5, 6, 7, 8]
+  const GET_REGISTERED_DONATIONS = gql`
+    {
+      stemCellDonationTokenizeds {
+        timestamp
+        donor
+      }
+    }
+  `
+  const {
+    loading,
+    error,
+    data: registered,
+  } = useQuery(GET_REGISTERED_DONATIONS)
+
+  if (loading) {
+    return (
+      <div>
+        <h2>Loading...</h2>
+      </div>
+    )
+  }
+
+  if (error) return `Error! ${error}`
+
+  let allRegisteredDonations = []
+  const getData = () => {
+    registered.stemCellDonationTokenizeds.forEach((item, index) => {
+      allRegisteredDonations.push({
+        timestamp: item.timestamp,
+        donor: item.donor,
+      })
+    })
+  }
+  const organizeByTime = () => {
+    allRegisteredDonations.sort((a, b) => {
+      return parseInt(b.timestamp) - parseInt(a.timestamp)
+    })
+  }
+  getData()
+  organizeByTime()
+
+  const registeredDonations = allRegisteredDonations.map((item, index) => (
+    <BioBankHistory key={index} item={item} />
+  ))
 
   return (
     <div className="w-full px-20 pt-5">
@@ -9,17 +53,17 @@ const BioBankHistoryList = () => {
         <div className=" font-satoshiMedium text-main basis-3/12">
           Date & Time
         </div>
-        <div className=" font-satoshiMedium text-main basis-5/12">
+        <div className=" font-satoshiMedium text-main basis-4/12">
           Donor address
         </div>
-        <div className=" font-satoshiMedium text-main basis-2/12">Amount</div>
+        <div className=" font-satoshiMedium text-main basis-[22%]">
+          Total Amount
+        </div>
         <div className=" font-satoshiMedium text-main basis-2/12 flex justify-center">
           Action
         </div>
       </div>
-      {appointments.map((element) => {
-        return <BioBankHistory key={element} />
-      })}
+      {registeredDonations}
     </div>
   )
 }
